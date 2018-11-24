ec776b601cda0b470677f77c01dec6a794871646
Shmat00ff <shmat00ff@gmail.com>
2018-11-24 23:26:12 +0700

    Last commit

--- project.py
+++ project.py
@@ -17,8 +17,26 @@ def pass_lang(x):
             password = choice(chars)
             signs.append(password)
 
+def int_pass(x):
+    if x == "yes":
+        n = int(input("How many numbers do you want to add?"))
+        chars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        for i in range(n):
+            password = choice(chars)
+            signs.append(password)
+
+def special_pass(y):
+    if y == "yes":
+        n = int(input("How many signs do you want to add?"))
+        chars = ["+", "-", "=", "$", "%", "!", "/", "#", "*", "?"]
+        for i in range(n):
+            password = choice(chars)
+            signs.append(password)
+
 def main():
     lang = pass_lang(input("Enter language of password(rus/eng):").lower())
+    inter = int_pass(str(input("Do you want to add numbers into your password(yes/no):")).lower())
+    special = special_pass(str(input("Do you want to add special signs into your password(yes/no):")).lower())
     shuffle(signs)
     pswrd = ""
     for i in range(len(signs)):
